56f89db4bf2daf7b3a5a8e3db661c55ba74c76bf
Peter Gemmell <74552992+Peter-Gemmell@users.noreply.github.com>
2022-04-01 14:34:28 -0400

    Changed to leaning guide #2

--- code.py
+++ code.py
@@ -10,14 +10,18 @@ import ugame
 
 def game_scene():
     # this function is the main game game_scene
-
-    print("\n\n\n")  # 3 blank lines
-    print("Hello World!")
+    
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
 
     # repeat, game loop
     while True:
-        pass  # just a place holder for now
-
-
+        pass # just a place holder for now
+    
 if __name__ == "__main__":
     game_scene()
+    

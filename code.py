4218a890c7de5d2dc05676f01d84f38fa5e50fdf
Peter Gemmell <74552992+Peter-Gemmell@users.noreply.github.com>
2022-04-14 20:48:45 -0400

    #6

--- code.py
+++ code.py
@@ -4,32 +4,38 @@
 # Created on: April 2022
 # This program is the "Space Aliens" program on the PyBadge
 
+import constants
 import stage
 import ugame
-
-import constants
 
 
 def game_scene():
     # this function is the main game game_scene
 
+    # image banks
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
+    # sets the background, 10x8
     background = stage.Grid(image_bank_background, 10, 8)
 
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
     )
 
+    # creates a stage, sets to 60fps
     game = stage.Stage(ugame.display, 60)
+    # order of layers
     game.layers = [ship] + [background]
+    # render the background and sprite list, most likely once per scene
     game.render_block()
 
-    # repeat, game loop
+    # repeat forever, game loop
     while True:
+        # get user input
         keys = ugame.buttons.get_pressed()
 
+        # A button to fire
         if keys & ugame.K_X != 0:
             print("A")
         if keys & ugame.K_O != 0:
@@ -52,12 +58,15 @@ def game_scene():
                 ship.move(0, ship.y)
 
         if keys & ugame.K_UP != 0:
-            pass
+            ship.move(ship.x, ship.y - 1)
         if keys & ugame.K_DOWN != 0:
-            pass
+            ship.move(ship.x, ship.y + 1)
 
+        # update game logic
+
+        # redraw Sprites
         game.render_sprites([ship])
-        game.tick()
+        game.tick()  # wait until refresh rate finishes
 
 
 if __name__ == "__main__":
--- cosntants.py
+++ cosntants.py
@@ -4,7 +4,6 @@
 # Created on: April 2022
 # This program is the constants file for "Space Aliens" program on the PyBadge
 
-# Pybadge screen size is 160x128 and sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -13,3 +12,11 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+  "button_up": "up",
+  "button_just_pressed": "just pressed",
+  "button_still_pressed": "still pressed",
+  "button_released": "released"
+}
